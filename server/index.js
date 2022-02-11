5817e62fa00fe489a8c5b855f90cb58e47dbb659
sumca252 <ismail.ali9315@gmail.com>
2022-02-11 11:25:20 +0100

    Added eslint, nodemon

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+/**
+ * Server
+ */
+"use strict";
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.listen(port, () => console.log(`Server started on port ${port}`));
