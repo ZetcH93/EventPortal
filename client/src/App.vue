008f8285c8a4444e5b21caa71b7e95f43ff0cb55
Mathilda Holmström <mathilda.holmstrom@telia.com>
2022-02-16 11:01:40 +0100

    If applied, this will fix the navbar hamburger issue.
    This issue made the hamburger menu not working, as it did not want to expand.

--- client/src/App.vue
+++ client/src/App.vue
@@ -14,7 +14,6 @@ export default {
 </script>
 
 <style>
-@import '~bootstrap/dist/css/bootstrap.css';
 @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
 
 #app {
