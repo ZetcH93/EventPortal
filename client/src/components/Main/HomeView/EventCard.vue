fdbe58855dc1b108814f44c505f45948d93f4522
Mathilda Holmström <mathilda.holmstrom@telia.com>
2022-02-16 22:40:11 +0100

    If applied, this will add some cards for the homepage.

---
+++ client/src/components/Main/HomeView/EventCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-for="event in events" :key="event.id" class="col">
+    <div class="card ">
+      <img :src="image" alt="test" class="card-img-top">
+      <div class="card-body shadow-sm rounded">
+        <h5 class="card-title">{{ event.name }}</h5>
+        <p class="card-text">{{ event.description }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import canoe from '@/assets/images/canoe.jpg'
+
+export default {
+  name: "EventCard",
+  props: ['events'],
+  data() {
+    return {
+      image: canoe
+    }
+  }
+}
+</script>
+
+<style scoped>
+.card {
+  z-index: 1;
+}
+</style>
--- client/src/components/Main/HomeView/Events.vue
+++ client/src/components/Main/HomeView/Events.vue
@@ -1,14 +1,55 @@
 <template>
   <h2>Events</h2>
-
+  <div class="event-cards">
+    <div class="row row-cols-2 row-cols-md-2 g-4 align-items-center">
+      <EventCard :events="events"/>
+    </div>
+  </div>
 </template>
 
 <script>
+import EventCard from "@/components/Main/HomeView/EventCard";
+
 export default {
-  name: "Events"
+  name: "Events",
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: [
+        {
+          'name': 'Event one',
+          'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris orci nibh, fringilla id consequat vel, accumsan ut nisi. Nullam ultricies velit sit amet ipsum faucibus, id malesuada est interdum. Suspendisse vitae erat et nunc iaculis dapibus vitae in elit. Proin.'
+        },
+        {
+          'name': 'Event two',
+          'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris orci nibh, fringilla id consequat vel, accumsan ut nisi. Nullam ultricies velit sit amet ipsum faucibus, id malesuada est interdum. Suspendisse vitae erat et nunc iaculis dapibus vitae in elit. Proin.'
+        },
+        {
+          'name': 'Event three',
+          'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris orci nibh, fringilla id consequat vel, accumsan ut nisi. Nullam ultricies velit sit amet ipsum faucibus, id malesuada est interdum. Suspendisse vitae erat et nunc iaculis dapibus vitae in elit. Proin.'
+        },
+        {
+          'name': 'Event four',
+          'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris orci nibh, fringilla id consequat vel, accumsan ut nisi. Nullam ultricies velit sit amet ipsum faucibus, id malesuada est interdum. Suspendisse vitae erat et nunc iaculis dapibus vitae in elit. Proin.'
+        },
+      ]
+    }
+  }
 }
 </script>
 
 <style scoped>
+.event-cards {
+  width: 60rem;
+  margin: auto auto -7rem;
+}
 
+@media only screen and (max-width: 600px) {
+  .event-cards {
+    width: 20rem;
+    margin: auto auto -7rem;
+  }
+}
 </style>
