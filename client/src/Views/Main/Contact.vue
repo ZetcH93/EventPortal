a9beba9cd31735ad2bbb4a44fcad2d22dcfd5e0a
Stina <stinagustafsson1@hotmail.com>
2022-02-18 15:24:15 +0100

    If apply: The contact, about and organization-pages will be updated.
    They look similar to the design in figma. However, the problem that's about the duplication of the body is still remaining.

--- client/src/Views/Main/Contact.vue
+++ client/src/Views/Main/Contact.vue
@@ -1,5 +1,31 @@
-<template>
-  <h1>Contact</h1>
+<template><br>
+
+  <br>
+  <br>
+  <div class="container-sm ">
+    <br>
+    <div>
+      <h5>Got any questions or concerns? Fill in the form and tell us about your issue and we'll get in touch with you
+        soon.</h5>
+    </div>
+    <div class="col-sm">
+      <div class="mb-3">
+        <input id="exampleFormControlInput1" class="form-control" placeholder="Name" type="text">
+      </div>
+      <div class="mb-3">
+        <input type="email" class="form-control" id="exampleFormControlInput2" placeholder="Email">
+      </div>
+      <div class="mb-3">
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="10" placeholder="Message"></textarea>
+      </div>
+      <br>
+      <div class="d-grid gap-2 col-6 mx-auto">
+        <button class="btn" type="button">Send message</button>
+      </div>
+      <br>
+    </div>
+  </div>
+  <br>
 </template>
 
 <script>
@@ -9,5 +35,19 @@ export default {
 </script>
 
 <style scoped>
+.container-sm {
+  box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.1);
+  padding: 10px;
+
+}
+
+.btn {
+  background-color: #4CAF50;
+  color: white;
+}
+
+.col-sm {
+  padding: 20px;
+}
 
 </style>
