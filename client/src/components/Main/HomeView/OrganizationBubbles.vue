fe90ecd3b2918ce83b968f872303d17adb84bf7f
Stina <stinagustafsson1@hotmail.com>
2022-02-18 20:12:17 +0100

    If apply: Home-page will have organization-bubbles.
    I only did changes in the OrganizationBubbles-file. I tried to implement it in the same way as the event-cards. But I didn't manage to fix that yet so by the time being we just have some figures to represent the organisations so far.

--- client/src/components/Main/HomeView/OrganizationBubbles.vue
+++ client/src/components/Main/HomeView/OrganizationBubbles.vue
@@ -1,14 +1,64 @@
 <template>
   <h2>Organizations</h2>
+  <div class="container ">
+    <div class="row row-cols-6">
+      <div class="col ">
+        <figure class="figure">
+          <img src="../../../assets/images/org1.jpg" class="figure-img img-fluid rounded-circle" alt="...">
+          <figcaption class="figure-caption">Organization 1</figcaption>
+        </figure>
+      </div>
+
+      <div class="col">
+        <figure class="figure">
+          <img src="../../../assets/images/org1.jpg" class="figure-img img-fluid rounded-circle" alt="...">
+          <figcaption class="figure-caption">Organization 2</figcaption>
+        </figure>
+      </div>
+
+      <div class="col ">
+        <figure class="figure">
+          <img src="../../../assets/images/org1.jpg" class="figure-img img-fluid rounded-circle" alt="...">
+          <figcaption class="figure-caption">Organization 3</figcaption>
+        </figure>
+      </div>
+
+
+      <div class="col ">
+        <figure class="figure">
+          <img src="../../../assets/images/org1.jpg" class="figure-img img-fluid rounded-circle" alt="...">
+          <figcaption class="figure-caption">Organization 4</figcaption>
+        </figure>
+      </div>
+
+      <div class="col bubble-hide">
+        <figure class="figure">
+          <img src="../../../assets/images/org1.jpg" class="figure-img img-fluid rounded-circle" alt="...">
+          <figcaption class="figure-caption">Organization 5</figcaption>
+        </figure>
+      </div>
+
+      <div class="col bubble-hide">
+        <figure class="figure">
+          <img src="../../../assets/images/org1.jpg" class="figure-img img-fluid rounded-circle" alt="...">
+          <figcaption class="figure-caption">Organization 6</figcaption>
+        </figure>
+      </div>
+    </div>
+    <br><br><br><br>
+  </div>
 </template>
 
 <script>
+
 export default {
   name: "OrganizationBubbles"
 }
+
 
 </script>
 
 <style scoped>
 
 </style>
+
