b7e840dae2f9efe67ed81bc37614ab3e4ef44217
Mathilda Holmström <mathilda.holmstrom@telia.com>
2022-02-18 16:18:37 +0100

    If applied, this will update the About text.
    The <br><br> has been changed to p-tags.

--- client/src/Views/Main/About.vue
+++ client/src/Views/Main/About.vue
@@ -2,23 +2,25 @@
   <br>
   <div class="container-sm text">
     <h1>EventPortal </h1>
-    <p class="text">This page is made by a group of students from BTH. <br><br>
-      Integer quis convallis nulla, consectetur venenatis leo. Phasellus blandit ornare leo nec auctor. Nulla lacinia
+    <p class="text"><small>This page is made by a group of students from BTH.</small></p>
+
+    <p>Integer quis convallis nulla, consectetur venenatis leo. Phasellus blandit ornare leo nec auctor. Nulla lacinia
       orci eu condimentum sollicitudin. Quisque et neque quis lectus euismod ullamcorper. Curabitur malesuada elit
       ligula, id convallis felis sodales eu. Cras velit leo, blandit tincidunt tellus ac, sagittis facilisis lectus.
       Donec ullamcorper suscipit nulla ut aliquam. Sed felis augue, rhoncus eget tortor lacinia, aliquam venenatis diam.
       Ut porttitor, quam non ullamcorper ultrices, nulla ipsum blandit libero, nec fermentum turpis nisi in neque.
       Pellentesque auctor vehicula erat, et mollis enim facilisis quis. Integer pharetra magna quis egestas dapibus.
       Donec quis ipsum pharetra, commodo dui et, pretium erat. Vestibulum suscipit tempus sapien, at tincidunt felis
-      bibendum vitae. Vivamus suscipit tellus aliquam lacinia commodo. Donec in semper urna.<br><br>
+      bibendum vitae. Vivamus suscipit tellus aliquam lacinia commodo. Donec in semper urna.</p>
 
+    <p>
       Aliquam volutpat tellus at lacus pellentesque iaculis. Sed eget tellus fringilla, sagittis ligula sed, facilisis
       dui. Sed pulvinar venenatis ligula. Etiam vitae hendrerit nisl. Aenean eget lobortis sapien. Mauris vel dolor
       velit. Fusce sit amet semper sapien. Fusce vel mattis metus. Donec ut congue ex. Pellentesque at dolor quam.
       Vestibulum vel ipsum orci. Sed eu est accumsan, facilisis nunc blandit, varius diam. Integer ultrices sodales
-      auctor. Ut nec sollicitudin sapien. Aenean facilisis volutpat erat ut iaculis.<br><br>
+      auctor. Ut nec sollicitudin sapien. Aenean facilisis volutpat erat ut iaculis.</p>
 
-      Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque id gravida
+    <p> Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque id gravida
       velit. Etiam luctus nibh nec leo mattis semper. Nulla facilisi. Nullam viverra id augue in commodo. In ultricies,
       mi id faucibus aliquam, mauris nibh dignissim libero, a dignissim risus nunc in lacus. Curabitur ac velit non leo
       feugiat fringilla quis in nisi. Sed non dapibus arcu, id vehicula nisl. Suspendisse et massa mollis, porta massa
